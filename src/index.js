e82dc66021eaf250ba574d5197d3a618381f90bf
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-25 10:07:54 -0400

    creating static html with table cards value suit etc

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
-import main from "./js/main"
-// const {
-//   default: { singleDeckGame }
-// } = require("blackjack-dealer-logic");
+const {
+  default: { singleDeckGame }
+} = require("blackjack-dealer-logic");
 
-alert("HELLO!");
 
-const buttons = document.querySelectorAll(".button");
 
-buttons.forEach(function (button) {
-
-  button.addEventListener("click", () => {
-
-    const paragraph = document.createElement("p");
-
-    paragraph.classList.add("newParagraph");
-
-    paragraph.textContent = "My silly paragraph!";
-
-    document.body.append(paragraph);
-
-  });
-
-  // backgroundButton.addEventListener("click", () => {
-  //   const paragraph = document.querySelector(".newParagraph");
-
-  //   paragraph.classList.add("redBackground");
-  //   paragraph.classList.remove("newParagraph");
-  // });
-
-  main()
