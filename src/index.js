fa70e3d37c020dc79033110a4e92a78659c81f74
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-26 17:08:30 -0400

    first javascript working

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@ const {
   default: { singleDeckGame }
 } = require("blackjack-dealer-logic");
 
+singleDeckGame.deal();
 
+var userHandStr = singleDeckGame.getUserHandValue();
 
+var userCardsArr = userHandStr.split(",")
+// for each card in userCardsArr {}
+  generateCard(userCardsArr[0]);
+  generateCard(userCardsArr[1]);
+//}
+//generateCard(userHand.getCards()[1]);
+
+function generateCard(card) {
+  const playingCard = document.createElement("section");
+  playingCard.classList.add("playing-card");
+
+  const valueContainer = document.createElement("section");
+  valueContainer.classList.add("value-container");
+
+  const value = document.createElement("span");
+  value.classList.add("value");
+  value.textContent = card;
+
+  valueContainer.append(value);
+  playingCard.append(valueContainer);
+
+  const table = document.querySelector(".table");
+  table.append(playingCard);
+}
