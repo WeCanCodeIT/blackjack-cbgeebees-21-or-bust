fdd63a0d102dc665692603db601b971044b8fca9
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-27 20:01:25 -0400

    broke it but we're trying to reset game

--- src/index.js
+++ src/index.js
@@ -4,35 +4,17 @@ const {
 
 const Dom = require("./utils/Dom");
 
-singleDeckGame.deal();
-
-let userChips = 200;
-let answer = prompt("How much would you like to bet?\nYour current chip count is "+userChips);
-
-let userCardsArray = singleDeckGame.getUserHand().getCards();
-let dealerCardsArray = singleDeckGame.getDealerHand().getCards();
-
 const userSection = document.querySelector(".hand-container-user");
 const dealerSection = document.querySelector(".hand-container-dealer");
 
-playingCardUser1 = Dom.generateCard(userCardsArray[0]);
-playingCardUser2 = Dom.generateCard(userCardsArray[1]);
-userSection.append(playingCardUser1);
-userSection.append(playingCardUser2);
-
-playingCardDealer1 = Dom.generateCard(dealerCardsArray[0]);
-playingCardDealer2 = Dom.generateCard(dealerCardsArray[1]);
-dealerSection.append(playingCardDealer1);
+Dom.renderHands(singleDeckGame,userSection,dealerSection);
 
 const hitButton = document.querySelector(".button-hit");
 hitButton.addEventListener("click", () => {
-  console.log ("hit button clicked");
-  newUserCard = Dom.hitButtonEvent(singleDeckGame);
+  console.log("hit button clicked");
+  newUserCard = Dom.hitButtonEvent(singleDeckGame,userSection,dealerSection);
   userSection.append(newUserCard);
-  // if (singleDeckGame.isUserBust() {
-  //   //end game and user loses
-  // }
-  
+
 })
 
 
--- src/utils/Dom.js
+++ src/utils/Dom.js
@@ -18,11 +18,48 @@ module.exports = {
     return playingCard;
   },
 
-  hitButtonEvent(singleDeckGame) {
+  hitButtonEvent(singleDeckGame, userSection, dealerSection) {
     singleDeckGame.hitUser();
     let userCardsArray = singleDeckGame.getUserHand().getCards();
     playingCardUser = this.generateCard(userCardsArray[userCardsArray.length - 1]);
+    // evaluate if user is bust, if so, user loses
+    if (singleDeckGame.evaluateUser() > 21) {
+      console.log("user busted");
+      result = singleDeckGame.outcome();
+      console.log(result);
+      this.renderHands(singleDeckGame,userSection,dealerSection);
+      return playingCardUser;
+    }
     return playingCardUser;
   },
+
+  renderHands(singleDeckGame,userSection,dealerSection) {
+
+    singleDeckGame.resetPlayers();
+    singleDeckGame.resetAnte();
+
+    singleDeckGame.deal();
+    userSection.valueContainer = "<span></span>";
+    //dealerSection
+    let userChips = document.querySelector(".chips").innerHTML;
+    let userAnte = prompt(
+      "How much would you like to bet?\nYour current chip count is "
+      + userChips);
+    userChips -= userAnte;
+    document.querySelector(".ante").innerHTML = userAnte;
+    document.querySelector(".chips").innerHTML = userChips;
+
+    let userCardsArray = singleDeckGame.getUserHand().getCards();
+    let dealerCardsArray = singleDeckGame.getDealerHand().getCards();
+
+    playingCardUser1 = this.generateCard(userCardsArray[0]);
+    playingCardUser2 = this.generateCard(userCardsArray[1]);
+    userSection.append(playingCardUser1);
+    userSection.append(playingCardUser2);
+
+    playingCardDealer1 = this.generateCard(dealerCardsArray[0]);
+    playingCardDealer2 = this.generateCard(dealerCardsArray[1]);
+    dealerSection.append(playingCardDealer1);
+  }
 
 }
